28a4ef8dff9dd3dd1879c245d4d51b15a6259c6e
0zyl02 <51076404+0zyl02@users.noreply.github.com>
2022-09-16 14:25:02 +0800

    提交

---
+++ vuex-course/src/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    //属性
+    count: 0,
+    todos: [
+      { id: 1, title: "todo item 1", completed: false },
+      { id: 2, title: "todo item 2", completed: false },
+      { id: 3, title: "todo item 3", completed: false }
+    ]
+  },
+  getters: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
